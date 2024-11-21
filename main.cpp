394949033c947b4916bc3f4c846002f1b72729b4
ahmed <ahmed.madeh.noah@gmail.com>
2024-11-21 17:03:19 +0200

    Completed the problem structure

--- main.cpp
+++ main.cpp
@@ -3,23 +3,42 @@
 
 using namespace std;
 
-const float startingInterval[2] = {0, 3};
-const float relativeErrorPercentage = 0.05;
+const float startingInterval[2] = {0, 3}; // change to search in another interval
+const float relativeErrorPercentage = 0.05; // change to get quicker or more accurate solutions
+const string function_of_x_str = "x - cos(x)"; // change for the printing
+unsigned int iterations = 0;
 
 float function_of_x(float x);
 
-bool has_solution(const float interval[2]);
+bool has_solution(const float SolutionInterval[2]);
+
+float average(const float SolutionInterval[2]);
+
+void bisect(float prev_x);
 
 int main() {
-    cout << has_solution(startingInterval) << endl;
+    cout << "Solving f(x) = " + function_of_x_str << endl
+            << "In the interval [" << startingInterval[0] << "," << startingInterval[1] << "]" << endl
+            << "With a relative error percentage of " << relativeErrorPercentage << "%" << endl;
+    float interval[2] = {startingInterval[0], startingInterval[1]};
+    bisect(interval[0]);
+    return 0;
 }
 
 
 float function_of_x(const float x) {
-    return x - cos(x);
+    return x - cos(x); // change the function you want to find the roots of
 }
 
 
-bool has_solution(const float interval[2]) {
-    return function_of_x(interval[0]) * function_of_x(interval[1]) < 0;
+bool has_solution(const float SolutionInterval[2]) {
+    return function_of_x(SolutionInterval[0]) * function_of_x(SolutionInterval[1]) < 0;
+}
+
+
+float average(const float SolutionInterval[2]) {
+    return (SolutionInterval[0] + SolutionInterval[1]) / 2;
+}
+
+void bisect(const float prev_x) {
 }
