44937fdead71d221ccf1d706d734e58e18a149c4
ahmed <ahmed.madeh.noah@gmail.com>
2024-11-21 17:32:20 +0200

    bisect() Implementation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 const float startingInterval[2] = {0, 3}; // change to search in another interval
 const float relativeErrorPercentage = 0.05; // change to get quicker or more accurate solutions
 const string function_of_x_str = "x - cos(x)"; // change for the printing
-unsigned int iterations = 0;
 
 float function_of_x(float x);
 
@@ -14,14 +13,14 @@ bool has_solution(const float SolutionInterval[2]);
 
 float average(const float SolutionInterval[2]);
 
-void bisect(float prev_x);
+void bisect(float interval[2], float prev_x = startingInterval[0], unsigned int iterations = 0);
 
 int main() {
     cout << "Solving f(x) = " + function_of_x_str << endl
             << "In the interval [" << startingInterval[0] << "," << startingInterval[1] << "]" << endl
-            << "With a relative error percentage of " << relativeErrorPercentage << "%" << endl;
+            << "With a relative error percentage of " << relativeErrorPercentage << "%" << endl << endl << endl;
     float interval[2] = {startingInterval[0], startingInterval[1]};
-    bisect(interval[0]);
+    bisect(interval);
     return 0;
 }
 
@@ -40,5 +39,31 @@ float average(const float SolutionInterval[2]) {
     return (SolutionInterval[0] + SolutionInterval[1]) / 2;
 }
 
-void bisect(const float prev_x) {
+void bisect(float interval[2], float prev_x, unsigned int iterations) {
+    iterations++;
+    const float average_x = average(interval);
+    const float mapped_interval[2] = {function_of_x(interval[0]), function_of_x(interval[1])};
+    const float mapped_average_x = function_of_x(average_x);
+    const float error = abs(((average_x - prev_x) / average_x) * 100);
+    cout << "Iteration: " << iterations
+            << "\nx-left: " << interval[0]
+            << "\nx-average: " << average_x
+            << "\nx-right: " << interval[1]
+            << "\nf(x-left): " << mapped_interval[0]
+            << "\nf(x-average): " << mapped_average_x
+            << "\nf(x-right): " << mapped_interval[1]
+            << "\nRelative Error Percentage: " << error << '%' << endl << endl << endl;
+    if (error < relativeErrorPercentage) {
+        cout << "The root equals " << average_x << " with a relative error percentage of " << error << '%' << endl
+                << "It was found in " << iterations << " iterations" << endl;
+    } else {
+        const float nextInterval[2] = {interval[0], average_x};
+        if (has_solution(nextInterval)) {
+            interval[1] = average_x;
+            bisect(interval, average_x, iterations);
+        } else {
+            interval[0] = average_x;
+            bisect(interval, average_x, iterations);
+        }
+    }
 }
