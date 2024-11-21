62c5f83d261d08134aac9ade48bd8a9845995d26
ahmed <ahmed.madeh.noah@gmail.com>
2024-11-21 16:38:58 +0200

    Defined solution interval checker

--- main.cpp
+++ main.cpp
@@ -8,10 +8,18 @@ const float relativeErrorPercentage = 0.05;
 
 float function_of_x(float x);
 
+bool has_solution(const float interval[2]);
+
 int main() {
+    cout << has_solution(startingInterval) << endl;
 }
 
 
 float function_of_x(const float x) {
     return x - cos(x);
+}
+
+
+bool has_solution(const float interval[2]) {
+    return function_of_x(interval[0]) * function_of_x(interval[1]) < 0;
 }
