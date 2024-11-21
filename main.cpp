d5d202947eb672f9c17f68856dcb6ba132a05ef0
ahmed <ahmed.madeh.noah@gmail.com>
2024-11-21 16:37:15 +0200

    Defined a problem's givens

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+const float startingInterval[2] = {0, 3};
+const float relativeErrorPercentage = 0.05;
+
+float function_of_x(float x);
+
 int main() {
-    cout << "Hello, World!!" << endl;
+}
+
+
+float function_of_x(const float x) {
+    return x - cos(x);
 }
